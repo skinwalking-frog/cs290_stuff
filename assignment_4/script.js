b44c2a26bad46957e8fd393ee8ef736172d437c3
skinwalking-frog <146015517+skinwalking-frog@users.noreply.github.com>
2024-03-12 16:16:23 -0700

    settings menu now toggles

--- assignment_4/script.js
+++ assignment_4/script.js
@@ -7,3 +7,19 @@ function Toggle_Menu(){
         document.getElementById("sidebar").style.display = "none";
     }
 }
+
+function Toggle_Themes_Menu(){
+    if(document.getElementById("themes").style.display == "none"){
+        document.getElementById("themes").style.display = "block";
+    }else{
+        document.getElementById("themes").style.display = "none";
+    }
+}
+
+function Light_Theme(){
+    let boxs = document.getElementsByClassName("entry");
+    for(let i = 0; i < boxs.length; i++){
+        boxs[i].style.backgroundColor = "dark-grey";
+    };
+    
+}
